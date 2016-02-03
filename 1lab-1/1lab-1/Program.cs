5ff14fb25eb32f6ab60b4708b6629401102ef45d
Darkhan022797 <Darkhan022797@gmail.com>
2016-02-04 02:32:26 +0600

    1 task

---
+++ 1lab-1/1lab-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, j;
+            n = 0;
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            for (i = a; i <= b; i++)
+            {
+                for (j = 1; j <= i; j++) 
+                {
+                    if (i % j == 0)
+                        n++;
+                }
+                if (n <= 2)
+                {
+                    Console.Write("{0} ",i);
+                }
+                n = 0;
+            }
+        Console.ReadKey();
+            }
+
+    }
+}
